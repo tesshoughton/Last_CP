83c41a93f51996de2da3175836f47cdac31feabf
EC2 Default User <ec2-user@ip-172-31-33-206.us-east-2.compute.internal>
2019-12-06 16:34:14 +0000

    About page, updated html for tagsearch

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>What are we all about?</h1>
+    <p>Too often, we forget that we are all humans. We all have stories that have shaped us over the years and it would benefit us all to remind ourselves of what makes us us.</p>
+    <p>That's why we here at <i>Tell Your Story</i> have built this website. Our goal is to bring greater unity to the human race by helping people understand each other's backgrounds better.</p>
+    <hr>
+    <img src="https://miro.medium.com/max/1024/1*Dv2-U8qdEsHFAKQEZ2Xs0Q.png">
   </div>
 </template>
+
+<style type="text/css">
+  img {
+    width: 100%;
+    text-align: center;
+  }
+</style>
