a220540b5b22d9d0afddbc4d2dc1ac2782162017
EC2 Default User <ec2-user@ip-172-31-33-206.us-east-2.compute.internal>
2019-12-06 16:47:43 +0000

    Connecting filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
 <div>
   <h1>Shared Stories</h1>
   <form>Filter by Tag: <input type="text" name="tagSearch"/></form>
-  <div v-for="story in stories" v-bind:key="story._id">
-    <hr>
-    <div class="story">
-      <div class="problem">
-        <p>{{story.problem}}</p>
-        <p><i>-- {{story.name}}</i></p>
+  <div class="stories" v-if="stories.length > 0">
+    <div v-for="story in stories" v-bind:key="story._id">
+      <hr>
+      <div class="story">
+        <div class="problem">
+          <p>{{story.problem}}</p>
+          <p><i>-- {{story.name}}</i></p>
+        </div>
       </div>
     </div>
   </div>
@@ -17,9 +19,14 @@
 <script>
 export default {
   name: 'home',
+  data(){
+    return {
+      tagSearch: "",
+    };
+  },
   computed: {
     stories() {
-      return this.$store.state.stories;
+      return this.$store.state.stories.filter(story => story.taggedWords.find(this.tagSearch));
     }
   },
   created() {
